6f06c07a74a5b7392086eac7dd0e5aa5764163d3
priscillay <priscilla@toast.co.nz>
2026-01-23 09:25:22 +1100

    update namespace and methods

--- code/IconFieldPathMigrator_Task.php
+++ code/IconFieldPathMigrator_Task.php
@@ -1,11 +1,16 @@
 <?php
 
-use SilverStripe\Core\ClassInfo;
+use SilverStripe\ORM\DB;
 use SilverStripe\Core\Convert;
 use SilverStripe\Dev\BuildTask;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\DB;
+use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Versioned\Versioned;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\PolyExecution\PolyOutput;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 
 class IconFieldPathMigrator_BuildTask extends BuildTask
 {
@@ -17,18 +22,21 @@ class IconFieldPathMigrator_BuildTask extends BuildTask
      * 4. Run this task - include params
      */
 
-    protected $title = 'Update icon file paths to assets folder';
+    protected string $title = 'Update icon file paths to assets folder';
     protected $enabled = true;
 
-    public function run($request)
+    protected function execute(InputInterface $input, PolyOutput $output): int
     {
+        $request = Injector::inst()->get(HTTPRequest::class);
         $vars = $request->getVars();
 
         if (!isset($vars['classname']) || !isset($vars['field'])) {
-            echo 'Pass both class and field in the query string, eg ?classname=Skeletor\DataObjects\SummaryPanel&field=SVGIcon' . '<br>';
-            echo 'If new folder is not \'SiteIcons\', pass new-path in the query string, eg &new-path=NewFolder' . '<br>';
-            echo 'Classname needs to include namespacing' . '<br>';
-            return;
+            $output->writeln('Please pass classname and field in the query string.');
+            $output->writeln('eg ?classname=Skeletor\DataObjects\SummaryPanel&field=SVGIcon');
+            $output->writeln('Pass both class and field in the query string, eg ?classname=Skeletor\DataObjects\SummaryPanel&field=SVGIcon');
+            $output->writeln('If new folder is not \'SiteIcons\', pass new-path in the query string, eg &new-path=NewFolder');
+            $output->writeln('Classname needs to include namespacing');
+            return Command::FAILURE;
         }
 
         $classname = $vars['classname'];
@@ -43,13 +51,15 @@ class IconFieldPathMigrator_BuildTask extends BuildTask
 
         // check if site is namespaced
         if (!ClassInfo::exists($classname)) {
-            die("Class $classname does not exist. Make sure to add the namespacing.");
+            $output->writeln("Class $classname does not exist. Make sure to add the namespacing.");
+            return Command::FAILURE;
         }
 
         $objects = $classname::get();
         $schema = DataObject::getSchema();
         if (!$schema->classHasTable($classname)) {
-            die("Class $classname does not have a table.");
+            $output->writeln("Class $classname does not have a table.");
+            return Command::FAILURE;
         }
         $tableName = Convert::raw2sql($schema->tableName($classname));// Sanitize column name
         $iconCol = Convert::raw2sql($iconField); // Sanitize column name
@@ -60,39 +70,39 @@ class IconFieldPathMigrator_BuildTask extends BuildTask
                 // if there is an icon
                 if ($originIconPath = $object->$iconField) {
                     $originIconName = basename($originIconPath);
-
-                    echo $object->Title . '<br>';
-                    echo 'Origin Icon Path: ' . $originIconPath . '<br>';
-                    echo 'Origin Icon Name: ' . $originIconName . '<br>';
+                    $output->writeln('Updating icon for ' . $object->Title);
+                    $output->writeln('Origin Icon Path: ' . $originIconPath);
+                    $output->writeln('Origin Icon Name: ' . $originIconName);
 
                     $newIconPath = $folderPath . '/' . $originIconName;
-                    echo 'New Icon Path: ' . $newIconPath . '<br>';
+                    $output->writeln('New Icon Path: ' . $newIconPath);
 
                     DB::prepared_query("UPDATE {$tableName} SET {$iconCol} = ? WHERE ID = ?", [$newIconPath, $object->ID]);
-                    echo $tableName.' updated' . '<br>';
+                    $output->writeln($tableName.' updated');    
 
                     if ($object->hasExtension(Versioned::class)) {
                         $tableNameVersioned = $tableName.'_Versions';
                         DB::prepared_query("UPDATE {$tableNameVersioned} SET {$iconCol} = ? WHERE RecordID = ?", [$newIconPath, $object->ID]);
-                        echo $tableNameVersioned . '<br>';
+                        $output->writeln($tableNameVersioned.' updated');
 
                         if ($object->isPublished()) {
                             $tableNameLive = $tableName.'_Live';
                             DB::prepared_query("UPDATE {$tableNameLive} SET {$iconCol} = ? WHERE ID = ?", [$newIconPath, $object->ID]);
-                            echo $tableNameLive . '<br>';
+                            $output->writeln($tableNameLive.' updated');
                         }
                     }
 
 
-                    echo 'panel icon updated' . '<br>';
+                    $output->writeln('panel icon updated');
                 } else {
-                    echo $object->Title . '<br>no icon - no update' . '<br>';
+                    $output->writeln($object->Title . ' no icon - no update');
                 }
 
-                echo '<br />-------<br />';
+                $output->writeln('-------');
             }
         } else {
-            echo 'No objects found';
+            $output->writeln('No objects found');
         }
+        return Command::SUCCESS;
     }
 }
